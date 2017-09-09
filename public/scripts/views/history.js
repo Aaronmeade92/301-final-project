5bd368b7fd9d317f88077e975bc587ee3cc7e5ab
Aaron <aaronmeade9292@gmail.com>
2017-09-09 14:29:48 -0700

    addded things

--- public/scripts/views/history.js
+++ public/scripts/views/history.js
@@ -1,6 +1,6 @@
 'use strict'
 app = app || {};
-// var Chart = require('chart.js');
+
 
 var chartProperties = function(){
   var labels = app.userData.map(function(day){
@@ -11,32 +11,6 @@ var chartProperties = function(){
   })
 }
 
-  var ctx = document.getElementById('myChart').getContext('2d');
-
-
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data:{
-      labels:'tacos',
-      datasets: [{
-        label: 'tacos',
-        data: 'data',
-        backgroundColor: [],
-        borderColor:[],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      events: ['click'],
-        scales:{
-          yAxes:[{
-            ticks:{
-              beginAtZero: true
-          }
-        }]
-      }
-    }
-  });
 
 
 var handleClick = function(event){
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 // const bodyParser = require('body-parser');
-
+// const Chart = require('chart.js');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
