89b5909889a7cf6e11a471d129b6e1d790dc2501
Aaron <aaronmeade9292@gmail.com>
2017-09-09 13:44:24 -0700

    added modules, server.js, started get requests

--- public/scripts/models/app.js
+++ public/scripts/models/app.js
@@ -1,1 +1,43 @@
 'use strict'
+
+var users = [];
+var app = app || {};
+var userData = [];
+
+(function (module){
+function Day(name, date, meals, sleep, meds, mood){
+  this.name = name;
+  this.date = date;
+  this.meals = meals;
+  this.sleep = sleep;
+  this.meds = meds;
+  this.mood = mood;
+}
+
+function User(name){
+  this.name = name;
+  this.data = [];
+  }
+
+User.fetchData = function(field, value){
+  ///get request for data
+  $.get('history/user', {field: field, val: value})
+  .then(results => {
+    results.forEach(function(day){
+      userData.push(new Day(day.name, day.date, day.meals, day.sleep, day.meds, day.mood));
+    })
+  })
+}
+
+///just testting chart data
+User.testPush = function(){
+userData.push(new Day('test', '10/03/2017', 'tacos', 8, 'taco meds', 3));
+}
+
+module.Day = Day;
+module.User = User;
+module.userData = userData;
+
+})(app);
+
+app.User.testPush();
--- public/scripts/views/history.js
+++ public/scripts/views/history.js
@@ -1,24 +1,33 @@
 'use strict'
+app = app || {};
+// var Chart = require('chart.js');
 
-var Chart = require('chart.js');
+var chartProperties = function(){
+  var labels = app.userData.map(function(day){
+    return day.date;
+  })
+  var data = app.userData.data.map(function(day){
+    return day.mood;
+  })
+}
 
-<script>
   var ctx = document.getElementById('myChart').getContext('2d');
+
 
   var myChart = new Chart(ctx, {
     type: 'bar',
     data:{
-      labels:[]
+      labels:'tacos',
       datasets: [{
-        label:
-        data:
-        backgroundColor: []
-        borderColor:[]
+        label: 'tacos',
+        data: 'data',
+        backgroundColor: [],
+        borderColor:[],
         borderWidth: 1
       }]
     },
     options: {
-      events: ['click'];
+      events: ['click'],
         scales:{
           yAxes:[{
             ticks:{
@@ -28,9 +37,9 @@ var Chart = require('chart.js');
       }
     }
   });
-</script>
 
-var handleClick(event){
+
+var handleClick = function(event){
   var activeElement = ctx.getElementAtEvent(event);
   console.log(activeElement);
 
