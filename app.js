a8822cd1352309442f5c4554497c3448af0d719f
Aaron <aaronmeade9292@gmail.com>
2017-09-09 11:31:12 -0700

    built constructor functions

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+var users = [];
+var app = {};
+
+(function (module){
+function Day(name, date, meals, sleep, meds, mood){
+  this.name = name;
+  this.date = date;
+  this.meals = meals;
+  this.sleep = sleep;
+  this.meds = meds;
+  this.mood = mood;
+}
+
+function User(name, data){
+  this.name = name;
+  this.data = data;
+  }
+
+}(app)
