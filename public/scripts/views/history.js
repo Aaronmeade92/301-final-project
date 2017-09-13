d79f281e0df3f9f26e426d6bd159bffa04ac3a20
Raegan Millhollin <desdemonhu@gmail.com>
2017-09-13 15:25:29 -0700

    Adding tooltip

--- public/scripts/views/history.js
+++ public/scripts/views/history.js
@@ -11,7 +11,7 @@ var userData = userData || [];
       return label.x;
     });
     app.dayData = userData.map(function(day){
-      return new app.Day(day);
+      return day.mood;
     })
 
     var ctx = document.getElementById("myChart").getContext('2d');
@@ -43,16 +43,81 @@ var userData = userData || [];
       },
       options: {
           scales: {
-            xAxes: [{
-                type: 'linear',
-                position: 'bottom'
-            }],
               yAxes: [{
                   ticks: {
                       beginAtZero:true
                   }
               }]
-          }
+          },
+          tooltips: {
+            custom: function(tooltipModel) {
+                // Tooltip Element
+                var tooltipEl = document.getElementById('chartjs-tooltip');
+
+                // Create element on first render
+                if (!tooltipEl) {
+                    tooltipEl = document.createElement('div');
+                    tooltipEl.id = 'chartjs-tooltip';
+                    tooltipEl.innerHTML = "<table></table>"
+                    document.body.appendChild(tooltipEl);
+                }
+
+                // Hide if no tooltip
+                if (tooltipModel.opacity === 0) {
+                    tooltipEl.style.opacity = 0;
+                    return;
+                }
+
+                // Set caret Position
+                tooltipEl.classList.remove('above', 'below', 'no-transform');
+                if (tooltipModel.yAlign) {
+                    tooltipEl.classList.add(tooltipModel.yAlign);
+                } else {
+                    tooltipEl.classList.add('no-transform');
+                }
+
+                function getBody(bodyItem) {
+                    return bodyItem.lines;
+                }
+
+                // Set Text
+                if (tooltipModel.body) {
+                    var titleLines = tooltipModel.title || [];
+                    var bodyLines = tooltipModel.body.map(getBody);
+
+                    var innerHtml = '<thead>';
+
+                    titleLines.forEach(function(title) {
+                        innerHtml += '<tr><th>' + title + '</th></tr>';
+                    });
+                    innerHtml += '</thead><tbody>';
+
+                    bodyLines.forEach(function(body, i) {
+                        var colors = tooltipModel.labelColors[i];
+                        var style = 'background:' + colors.backgroundColor;
+                        style += '; border-color:' + colors.borderColor;
+                        style += '; border-width: 2px';
+                        var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+                        innerHtml += '<tr><td>' + span + body + '</td></tr>';
+                    });
+                    innerHtml += '</tbody>';
+
+                    var tableRoot = tooltipEl.querySelector('table');
+                    tableRoot.innerHTML = innerHtml;
+                }
+
+                // `this` will be the overall tooltip
+                var position = this._chart.canvas.getBoundingClientRect();
+
+                // Display, position, and set styles for font
+                tooltipEl.style.opacity = 1;
+                tooltipEl.style.left = position.left + tooltipModel.caretX + 'px';
+                tooltipEl.style.top = position.top + tooltipModel.caretY + 'px';
+                tooltipEl.style.fontFamily = tooltipModel._fontFamily;
+                tooltipEl.style.fontSize = tooltipModel.fontSize;
+                tooltipEl.style.fontStyle = tooltipModel._fontStyle;
+                tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+            }
       }
     });
 
